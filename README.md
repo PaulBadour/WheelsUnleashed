2e93bb45470424c62401ea5a246e6ecab3448088
Paul Badour <paulrbadour@gmail.com>
2023-10-01 23:50:25 -0400

    Made basic layout and tested basic DB intefration

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WheelsUnleashed
+
+
+This is a project inspired by the game Sea of Stars, more specifically the the minigame "Wheels" inside of the game
+
+This my attempt at making a multiplayer version of this game to be played online using Django (my first time using it)
--- WheelsUnleashed/Wheels/views.py
+++ WheelsUnleashed/Wheels/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import TestModel
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, "base.html")
+
+def adminDB(request):
+    items = TestModel.objects.all()
+    return render(request, "testDB.html", {"items": items})
